f903da027038eba9983992dd879fc5e696e20f18
Sean Dunn <sedunn@adobe.com>
2015-08-04 19:45:15 -0400

    SuggestionArea: Always show 8 suggestions, including upload

--- src/js/SuggestionArea.js
+++ src/js/SuggestionArea.js
@@ -3,7 +3,13 @@ import View from 'BeFF/View';
 import template from '../templates/suggesstion-area.mustache';
 
 export default View.extend({
+  _maxSuggestions: 7,
+
   mustache: template,
+
+  init(model) {
+    this._super(this._padOrTruncateSuggestions(model));
+  },
 
   events: {
     click: {
@@ -12,7 +18,24 @@ export default View.extend({
     }
   },
 
+  _padOrTruncateSuggestions(model) {
+    model.suggestions = model.suggestions || [];
+
+    if (model.suggestions.length < this._maxSuggestions) {
+      while (model.suggestions.length < this._maxSuggestions) {
+        model.suggestions.push({ src: '' });
+      }
+    }
+    else {
+      model.suggestions = model.suggestions.slice(0, 7);
+    }
+
+    return model;
+  },
+
   _setImage({ currentTarget: target }) {
+    if (!target.dataset.src) { return; }
+
     this.trigger('set-image', target.dataset.src);
   }
 });
